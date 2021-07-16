edd2c1c35ff89446a470d93af305c08ccba996cd
ErickChAg <66395496+ErickChAg@users.noreply.github.com>
2021-07-15 21:16:31 -0600

    gui_changes

--- FrontEnd-Client-TeleAtlantico-Support-master/src/app/components/home/home.component.ts
+++ FrontEnd-Client-TeleAtlantico-Support-master/src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { Router } from '@angular/router';
+import { Client } from '../../models/client.model';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  client:Client;
+  constructor(private clientService: ClientService, private router:Router) { }
 
   ngOnInit(): void {
+    this.client = this.clientService.client;
   }
 
 }
